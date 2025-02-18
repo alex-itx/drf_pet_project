12e58d5dd41db2f4f1ef80d928042e37ff791a6a
alex-itx <aleksandr.ityaksov@gmail.com>
2025-02-18 18:10:47 +0300

    lesson 5 ListCreateAPIView and ModelSerializer

--- drfsite/women/serializers.py
+++ drfsite/women/serializers.py
@@ -1,47 +1,15 @@
 import io
-
 from rest_framework import serializers
 from rest_framework.parsers import JSONParser
 from rest_framework.renderers import JSONRenderer
-
 from .models import Women
 
 
-
-# селиализатор отнаследованный от базового класса Serializer
-class WomenSerializer(serializers.Serializer):
-    # Вручную пропишем поля для котороый будут сериализироваться
-    title = serializers.CharField(max_length=255)
-    content = serializers.CharField()
-    time_create = serializers.DateTimeField(read_only=True)
-    time_update = serializers.DateTimeField(read_only=True)
-    is_published = serializers.BooleanField(default=True)
-    cat_id = serializers.IntegerField()
-
-
-    def create(self, validated_data):
-        """
-        create() - метод для сохранения данных в бд
-            validated_data - принимает валидированные данные, которые создаются после валидации во вью
-        :return - создаёт запись в бд, при помощи ОРМ функции create
-        """
-        return Women.objects.create(**validated_data)
-
-
-    def update(self, instance, validated_data):
-        """
-        update() - используется для изменения записей
-            В него мы передаём каждое поле как объект instance, потом сохраняем его и возвращаем
-        :param instance: объект записи, через именнованный параметр указывается поле с тем же именем
-        :return: возвращает объект instance
-        """
-        instance.title =  validated_data.get('title', instance.title)
-        instance.content = validated_data.get('content', instance.content)
-        instance.time_update = validated_data.get('time_update', instance.time_update)
-        instance.is_published = validated_data.get('is_published', instance.is_published)
-        instance.cat_id = validated_data.get('cat_id', instance.cat_id)
-        instance.save()
-        return instance
+# Сериализатор для таблицы women
+class WomenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Women
+        fields = ['title', 'content', 'cat']
 
 
 
@@ -56,6 +24,45 @@ class WomenSerializer(serializers.Serializer):
 
 
 
+
+
+
+
+
+# # селиализатор отнаследованный от базового класса Serializer
+# class WomenSerializer(serializers.Serializer):
+#     # Вручную пропишем поля для котороый будут сериализироваться
+#     title = serializers.CharField(max_length=255)
+#     content = serializers.CharField()
+#     time_create = serializers.DateTimeField(read_only=True)
+#     time_update = serializers.DateTimeField(read_only=True)
+#     is_published = serializers.BooleanField(default=True)
+#     cat_id = serializers.IntegerField()
+#
+#
+#     def create(self, validated_data):
+#         """
+#         create() - метод для сохранения данных в бд
+#             validated_data - принимает валидированные данные, которые создаются после валидации во вью
+#         :return - создаёт запись в бд, при помощи ОРМ функции create
+#         """
+#         return Women.objects.create(**validated_data)
+#
+#
+#     def update(self, instance, validated_data):
+#         """
+#         update() - используется для изменения записей
+#             В него мы передаём каждое поле как объект instance, потом сохраняем его и возвращаем
+#         :param instance: объект записи, через именнованный параметр указывается поле с тем же именем
+#         :return: возвращает объект instance
+#         """
+#         instance.title =  validated_data.get('title', instance.title)
+#         instance.content = validated_data.get('content', instance.content)
+#         instance.time_update = validated_data.get('time_update', instance.time_update)
+#         instance.is_published = validated_data.get('is_published', instance.is_published)
+#         instance.cat_id = validated_data.get('cat_id', instance.cat_id)
+#         instance.save()
+#         return instance
 
 
 
@@ -101,23 +108,3 @@ class WomenSerializer(serializers.Serializer):
 #     serializer = WomenSerializer(data=data)
 #     serializer.is_valid()
 #     print(serializer.validated_data)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# # Сериализатор для таблицы women
-# class WomenSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Women
-#         fields = ['title', 'cat_id']
--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -4,68 +4,18 @@ from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
 from .models import Women
 from .serializers import WomenSerializer
 
-# Представление стандартного класса APIView
-class WomenAPIView(APIView):
-    def get(self, request):
-        """
-        метод get() - он принимает и обрабатывает get-запрос
-        w - список со всеми данными из таблицы Women
-        return - возвращает сериализованные данные
-            many=True передаёт значения списком
-        """
-        w = Women.objects.all()
-        return Response({'posts': WomenSerializer(w, many=True).data})
 
 
-
-    def post(self, request):
-        """
-        Описание - благодаря данному методу, мы можем передать словарь в json с ключами - названием полей
-        и значеним - значением полей, и таким образом создать новую запись в таблице
-        postman->post->Body->raw->json
-        метод  post() - он принимает и обрабатывает post-запрос
-        serializer - хранит в себе данные из запроса, чтобы потом валидировать их,
-        а потом сохранить как объект модели
-        return - возвращает данные добавленные в таблицу
-
-        """
-        serializer = WomenSerializer(data=request.data)
-        serializer.is_valid()
-        serializer.save()
-
-        return Response({'post': serializer.data})
-
-
-    def put(self, request, *args, **kwargs):
-        """
-        put() - используется для изменения записей в бд
-        в функции сначала определяется поле из запроса (равное конвертеру),
-        после проверяется есть ли оно в таблице,
-        если есть записываем строку с таким полем,
-        далее селиалезируем строку,
-        сохраняем её - вызываем метод update из сериализатора
-
-        :return: возвращаем сериализованные данные
-        """
-        pk = kwargs.get("pk", None)
-        if not pk:
-            return Response({"error": "Method PUT not allowed"})
-
-        try:
-            instance = Women.objects.get(pk=pk)
-        except:
-            return Response({"error": "Object does not exists"})
-
-        serializer = WomenSerializer(data=request.data, instance=instance)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({'post': serializer.data})
-
-
+# вью для таблицы women с использованием ListCreateAPIView
+class WomenAPIList(generics.ListCreateAPIView):
+    """
+    ListCreateAPIView - класс для чтения (get-запрос) и создания списка данных (post-запрос)
+    """
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
 
 
 
@@ -93,3 +43,64 @@ class WomenAPIView(APIView):
 #     queryset = Women.objects.all()
 #     # сериализатор
 #     serializer_class = WomenSerializer
+
+
+
+
+
+# # Представление стандартного класса APIView
+# class WomenAPIView(APIView):
+#     def get(self, request):
+#         """
+#         метод get() - он принимает и обрабатывает get-запрос
+#         w - список со всеми данными из таблицы Women
+#         return - возвращает сериализованные данные
+#             many=True передаёт значения списком
+#         """
+#         w = Women.objects.all()
+#         return Response({'posts': WomenSerializer(w, many=True).data})
+#
+#
+#
+#     def post(self, request):
+#         """
+#         Описание - благодаря данному методу, мы можем передать словарь в json с ключами - названием полей
+#         и значеним - значением полей, и таким образом создать новую запись в таблице
+#         postman->post->Body->raw->json
+#         метод  post() - он принимает и обрабатывает post-запрос
+#         serializer - хранит в себе данные из запроса, чтобы потом валидировать их,
+#         а потом сохранить как объект модели
+#         return - возвращает данные добавленные в таблицу
+#
+#         """
+#         serializer = WomenSerializer(data=request.data)
+#         serializer.is_valid()
+#         serializer.save()
+#
+#         return Response({'post': serializer.data})
+#
+#
+#     def put(self, request, *args, **kwargs):
+#         """
+#         put() - используется для изменения записей в бд
+#         в функции сначала определяется поле из запроса (равное конвертеру),
+#         после проверяется есть ли оно в таблице,
+#         если есть записываем строку с таким полем,
+#         далее селиалезируем строку,
+#         сохраняем её - вызываем метод update из сериализатора
+#
+#         :return: возвращаем сериализованные данные
+#         """
+#         pk = kwargs.get("pk", None)
+#         if not pk:
+#             return Response({"error": "Method PUT not allowed"})
+#
+#         try:
+#             instance = Women.objects.get(pk=pk)
+#         except:
+#             return Response({"error": "Object does not exists"})
+#
+#         serializer = WomenSerializer(data=request.data, instance=instance)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response({'post': serializer.data})
